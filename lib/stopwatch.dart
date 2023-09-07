15fd4f485ed28f1fc3090a90d01d84506deb090c
Wilson Aballey <willywallet001@gmail.com>
2023-09-07 08:41:34 +0000

    timer function commit

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class StopWatch extends StatefulWidget {
@@ -8,6 +10,23 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
+  late int seconds;
+  Timer? timer;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    seconds = 0;
+    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+  }
+
+  void _onTick(Timer timer) {
+    setState(() {
+      ++seconds;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
